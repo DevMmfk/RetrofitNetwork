6540af99662549d12291b768101155d5810415b2
Mohmmad Kamal <m.kamal@petraride.com>
2020-12-13 11:53:55 +0200

    add delete for apiService

--- app/src/main/java/com/network/app/MyApp.kt
+++ app/src/main/java/com/network/app/MyApp.kt
@@ -7,7 +7,7 @@ class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         /**  Set base url and headers for api */
-        Initialize.networkConfig("https://api.koko-stores.com/", getHeaderMap())
+        Initialize.networkConfig("https://...", getHeaderMap())
     }
 
     private fun getHeaderMap(): HashMap<String, String> {
@@ -15,7 +15,7 @@ class MyApp : Application() {
         /** if you want add header */
 
         headerMap["Authorization"] =
-            "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIyIiwianRpIjoiZDg2OGVmMzdiOGE0NTg0YTFlMDQ3MzEzMWI2OWYyMTAzOGMxMDQ3ZDVmNTU4MDAyODhjZWJlODcwNzExNjIwZTQ5NDc0YjdkNWQ3ZTFkODciLCJpYXQiOjE2MDc4NDc2NjksIm5iZiI6MTYwNzg0NzY2OSwiZXhwIjoxNjM5MzgzNjY5LCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.PB8oMcSLXHkd0hpAvVqCrB-2DvofS5jsViu4pJmgUE_gEspc7-_dyHb2ndoss1lOW8cJjJodhbG-DGT7HGNoAjNZLTQiRvITNQaqPK7Di2a3uViRs2L0sXU-4RfsVKRzUi9v-dK0omAFPHyaZFbv6RZB9BwkVs6UbZnukwHx1BjOPCDd76Nc-Uwj_zOXE5vmu8iMTEBjCwRFd0UDM_l519SDgqMnRiUgkeFcNDUQDMjFv_DB_9Z3SbDPf49cgTd08F2xcXIL-4VY84-fgL7Eh5NPzvWh4MV4W7e3S2mFFA9g8rAEthfOPDZQt-oqXdU7oNh_t2chgHpxmbu7eC_tey-CoIdGfjfdLoFJiI9NBu7811Hv1eRwLcYxg0mVd81ihU2y89iXI5VLWmJMFs89sfg5kynKP69cBQ2xRdA9d5s2vQXgyX8HSTZCVmaxkaLsurqY0LaE33kmxN22A76-I6wniGoP8SSR5ssnSxLRuMF1ptyHbbC5Vx40gGK3WDsL6icGSfNqP5UJ1eE6JWwLKNA8cPFSx2k-rs9M-2qcIYgywvmbmMVrnKDS_RptmfIjjE2jLiTRaxIkX54lfC9w3VlwLkhdMIHuXbV3ed3D5fkC4ZtFQf4QvkL9NNAU59uyqQ2JBVQq4OPXOxCWdOA-UuPq56LiywyGbcm-U0BKIbs"
+            "Bearer ..."
         return headerMap
     }
 }
--- app/src/main/java/com/network/layer/viewModel/NewViewModel.kt
+++ app/src/main/java/com/network/layer/viewModel/NewViewModel.kt
@@ -12,23 +12,20 @@ class NewViewModel(application: Application) : BaseNetwork(application) {
 
     fun getProfile(context: Context) {
         showDialog(context)
-        getData("users/v1/me", responseLiveData1)
+        getData("...", responseLiveData1)
     }
 
     fun login(context: Context) {
         showDialog(context)
         val map = HashMap<String, String>()
-        map["grant_type"] = "password"
-        map["client_id"] = "2"
-        map["client_secret"] = "gPENEIiNCCoFnFi9ibom1tdDRZblznIjmEa8bbZu"
-        map["username"] = "+962786184866"
+        map["username"] = "+962"
         map["password"] = "1234"
-        map["device_token"] = "f1c2tbZ6RL6adoMvDkndOQ:APA91bGUstdb-nmsmjqmN0Gn3KMEw9iFBLJRb90yWlsQQ7BaQtOrO869F5KXAtEVw5A3fZ8Pn7gBA_OVjcJvT1WLpxD3YoPHJwvN_GhEV47rKADxLqp84_qUkNqleb6AU1tOxhu_aVhN"
-        getData("users/v1/oauth/token", map, responseLiveData2)
+        map["device_token"] = "JRb90yWlsQQ7BaQtOrO869F5KXAtEVw5A3fZ8Pn7gBA_OVjcJvT1WLpxD3YoPHJwvN_GhEV47rKADxLqp84_qUkNqleb6AU1tOxhu_aVhN"
+        getData("...", map, responseLiveData2)
     }
 
     fun deleteP(context: Context) {
         showDialog(context)
-        deleteApi("stores/v1/products/17", deleteLiveData)
+        deleteApi("....", deleteLiveData)
     }
 }
